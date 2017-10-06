e1500e123313996d0d9cef21c0d01538e0524ee7
白石隆二 <shiraishiryuji@shiraishiryuujinoMacBook-Air.local>
2017-10-06 16:38:31 +0900

    add attendance_index

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -7,4 +7,10 @@ class Attendance < ActiveRecord::Base
   belongs_to :user
   accepts_nested_attributes_for :worktime_aggregates, allow_destroy: true
 
+  def sum_of_attendance_time
+    ((closing_datetime - opening_datetime) / 60 / 60 -1).to_f
+  end
+  def sum_of_over_time
+    ((closing_datetime - opening_datetime) / 60 / 60 -9).to_f
+  end
 end
