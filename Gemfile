0bd1e3340f7e23c02d632d00ae8ab310b437e0d1
白石隆二 <shiraishiryuji@shiraishiryuujinoMacBook-Air.local>
2017-12-02 19:33:31 +0900

    fixed_sum

--- Gemfile
+++ Gemfile
@@ -43,6 +43,7 @@ group :development, :test do
   gem 'spring'
   gem 'pry-rails'
   gem 'better_errors'
+  gem "binding_of_caller"
 end
 
 group :development do
--- app/models/worktime_aggregate.rb
+++ app/models/worktime_aggregate.rb
@@ -3,9 +3,11 @@ class WorktimeAggregate < ActiveRecord::Base
   belongs_to :construction
 
   def self.sum_of_constructiontimes(construction_id, start_date = nil, start_year = nil, start_month = nil) #constructiontime月集計
+
     if start_date == nil && start_year == nil && start_month = nil #default
       joins(:attendance).where("attendances.opening_datetime >= :start AND attendances.opening_datetime <= :end", {start: Time.zone.today.beginning_of_month, end: Time.zone.today.end_of_month}).where(construction_id: construction_id).sum(:constructiontime)
     else
+      #binding.pry
       end_date = Date.new(start_year,start_month,-1).to_s  #select
       joins(:attendance).where("attendances.opening_datetime >= :start AND attendances.opening_datetime <= :end", {start: start_date, end: end_date}).where(construction_id: construction_id).sum(:constructiontime)
     end
@@ -15,12 +17,15 @@ class WorktimeAggregate < ActiveRecord::Base
     results = []  #配列変数定義
     end_date = Date.new(start_year,start_month,-1) #paramsから月末計算
     end_day = (end_date).day #end_dateの日のみ取得
+    #binding.pry
     (start_date.to_date..start_date.to_date + (end_day-1)).each do |day|  #日付のループ取得
       result = {}   #ハッシュ変数定義
-      start_time = day.beginning_of_day.to_s #1日の始まり成形
-      end_time = day.end_of_day.to_s   #1日の終わり成形
+      start_time = (day.beginning_of_day).to_s #1日の始まり成形
+      end_time = (day.end_of_day).to_s   #1日の終わり成形
       result[:day] = day  #キーをdayとしてハッシュ内に入れる
+
       result[:time] = joins(:attendance).where("attendances.opening_datetime >= :start AND attendances.opening_datetime <= :end", {start: start_time, end:  end_time}).where(construction_id: construction_id).sum(:constructiontime)  #集計
+      #binding.pry
       results << result #ハッシュを配列内に代入
     end
     results  #return
