338d9ea1116e8c58d903b029bc1de7cf28bb33df
白石隆二 <shiraishiryuji@shiraishiryuujinoMacBook-Air.local>
2017-12-16 11:42:41 +0900

    delete_confirmable

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171216012455) do
+ActiveRecord::Schema.define(version: 20171216020632) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,10 +52,6 @@ ActiveRecord::Schema.define(version: 20171216012455) do
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.string   "name"
-    t.string   "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
     t.boolean  "admin",                  default: false
     t.string   "avatar"
   end
