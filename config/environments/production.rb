7d222675be2b621c2c6718da28e501a384cd80ad
白石隆二 <shiraishiryuji@shiraishiryuujinoMacBook-Air.local>
2017-12-21 10:08:04 +0900

    add_sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = {host: 'stormy-sierra-90103.herokuapp.com'}
+  config.action_mailer.default_url_options = {host: 'secret-beach-14321.herokuapp.com'}
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   {
